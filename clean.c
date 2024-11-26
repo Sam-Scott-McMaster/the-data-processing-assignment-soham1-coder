b3fc00cb2c1ffa566e5b8462da92a4c7ed80a1b8
soham <sohamhajariwala@outlook.com>
2024-11-26 13:30:28 -0500

    read_data function added

--- clean.c
+++ clean.c
@@ -1,5 +1,13 @@
+/*Soham Hajariwala, hajariws 11/27/2024
+
+This program cleans up data which wil be used for machine learning
+
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char *argv[]) {
 
@@ -12,11 +20,36 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-
-float read_data(int rows, int cols)
+//reads float values from std input and assigns them to a 2D array
+float **read_data(int *rows, int *cols)
 {
     scanf("%d %d", rows, cols); //obtains #of rows and columns from user
 
+    //allocates memory for the 2D array
+    float **data = (float**) malloc(*rows * sizeof(float*));
+
+    for(int i = 0; i < *rows; i++)
+    {
+        data[i] = (float*) malloc(*cols * sizeof(float));
+
+        for (int j = 0; j < *cols; j++)
+        {
+            float temp; //temporary variable to store the input
+
+            if (scanf("%f", &temp) != 1) 
+            {
+                data[i][j] = NAN;
+            } 
+            
+            //assigns valid numbers to the array
+            else 
+            {
+                data[i][j] = temp; 
+            }    
+        }    
+    }
+
+    return data;
 }
 
 float clean_delete(float *a[])
