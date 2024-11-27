1256bf301716835e9b42ee6d14954c142c2750f2
soham <sohamhajariwala@outlook.com>
2024-11-27 12:16:44 -0500

    added clean_impute

--- clean.c
+++ clean.c
@@ -79,7 +79,7 @@ float **read_data(int *rows, int *cols)
 float **clean_delete(float **data, int rows, int cols, int *new_rows)
 {
 
-    //allocates memory for a temporary array to store valid rows
+    //allocates memory for an array to store valid rows
     float **cleaned_data = (float**) malloc(rows * sizeof(float*));
 
     int valid_rows = 0; //number of valid rows
@@ -119,12 +119,65 @@ float **clean_delete(float **data, int rows, int cols, int *new_rows)
 }
 
 
-float clean_impute(float *a[])
+float **clean_impute(float **data, int rows, int cols)
 {
+    //allocates memory for a new array with cleaned data
+    float **cleaned_data = (float**) malloc(rows * sizeof(float*));
 
+    //Allocates memory for an array within the new array (2D array)
+    for (int i = 0; i < rows; i++) 
+    {
+        cleaned_data[i] = (float *)malloc(cols * sizeof(float));
+    }
+
+    //iterates column by column
+    for (int j = 0; j < cols; j++) 
+    {
+        float sum = 0;
+        float average = 0;
+        int nan_count = 0;
+
+        for (int i = 0; i < rows; i++) 
+        {
+            if (isnan(data[i][j])) 
+            {
+                nan_count++;
+            }
+
+            else
+            {
+                sum += data[i][j];
+            }
+        }
+        
+        if (rows - nan_count > 0) //avoids division by zero
+        {
+            average = sum / (rows - nan_count);
+        }
+
+        else
+        {
+            average = 0; //average for column of all NAN values
+        }
+
+        //clones the array while replacing NAN values in the column with the average
+        for (int i = 0; i < rows; i++) 
+        {
+            if (isnan(data[i][j])) 
+            {
+                cleaned_data[i][j] = average;
+            }
+
+            else
+            {
+                cleaned_data[i][j] = data[i][j];
+            }
+        }
+    }
+    return cleaned_data;
 }
 
-void output_data(float *a[])
+void output_data(float **data)
 {
 
 }
